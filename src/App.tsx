291bdd35ce94e576f8e747825065ad656e07ac11
Денис <denisegorenko1@gmail.com>
2022-06-23 20:13:09 +0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [value, setValue] = useState(0)
+
+    useEffect(()=>{
+        let valueStr = (localStorage.getItem('counterValue'))
+
+        if (valueStr){
+            setValue(JSON.parse(valueStr))
+        }
+    }, [])
+
+
+    useEffect(()=>{
+        localStorage.setItem('counterValue', JSON.stringify(value))
+    }, [value])
+
+    const incHandler = () => {
+        setValue(value + 1)
+    }
+
+    const clearLocalStorageHandler = () => {
+        setValue(0)
+    }
+    //
+    // const getFromLocalStorageHandler = () => {
+    //     setValue(Number(localStorage.getItem('counterValue')))
+    // }
+
+
+
+    return (
+        <div className="App">
+            <h1>{value}</h1>
+            <button onClick={incHandler}>inc</button>
+            {/*<button onClick={setToLocalStorageHandler}>setToLocalStorage</button>*/}
+            <button onClick={clearLocalStorageHandler}>clearLocalStorage</button>
+            {/*<button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>*/}
+        </div>
+    );
 }
 
 export default App;
